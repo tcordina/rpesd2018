8bda82b218f62744812803e995f986bdaa0ce8ac
Thibaud Cordina <contact@thibaudcordina.fr>
2018-03-22 00:13:24 +0100

    Intégration des vues connection inscription

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new \FOS\UserBundle\FOSUserBundle(),
             new Vich\UploaderBundle\VichUploaderBundle(),
             new Ornicar\GravatarBundle\OrnicarGravatarBundle(),
+            new UserBundle\UserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
